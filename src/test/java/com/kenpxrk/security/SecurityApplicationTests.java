0574a406679ce2639c730c002a6ccd6d9adcd73d
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-10-10 13:44:03 +0300

    init

---
+++ src/test/java/com/kenpxrk/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenpxrk.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
